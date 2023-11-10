aa6558735c9bfe6c9692ed7e5ef253791bb54997
liang452 <143743607+liang452@users.noreply.github.com>
2023-11-10 10:00:45 -0500

    Update Store.java

--- src/Store.java
+++ src/Store.java
@@ -1,1 +1,14 @@
+public class Store {
+private String name;
+private ArrayList<Product> products;
 
+  public Store(String name) {
+    //declares without file; manually adds products later
+    this.name = name;
+  }
+  public Store(String name, String filename) {
+     this.name = name;
+//TODO: read from file, add to products list
+
+}
+}
